aea6498a3200d5a995185d3df6151e505ba52bd9
Ilse Romero <ilseromero35@gmail.com>
2026-09-22 06:06:53 -0400

    fix(environment): update apiBaseUrl to use production endpoint

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -33,7 +33,7 @@ const defaultFirebaseConfig: FirebaseConfig = {
 };
 
 export const environment = {
-  apiBaseUrl: runtimeConfig.apiBaseUrl || 'http://localhost:8000/api',
+  apiBaseUrl: runtimeConfig.apiBaseUrl || 'https://api-ecommerce.devhooh.com/api',
   decartApiKey: runtimeConfig.decartApiKey || 'dct_ecommerce_QZtgfQYtsNcdcedjtHBqrfDMqkUMCFVEwKSzqdzTkVTgSPLlqGPwlOzXrDoxQFyX',
   stripePublishableKey: runtimeConfig.stripePublishableKey || 'pk_test_51PwuiM08hp2qIPTJ9P4c108993LSovebHw9lQQeABXF3zkN71Upef4jMuPMgLPjJDWOpL5N2I94cMtze0nOxg9IP00Jo5RqrJ7',
   firebase: { ...defaultFirebaseConfig, ...runtimeConfig.firebase },
